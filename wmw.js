e55652b564f03c03a5eb09bccbc495d60464ea57
pcn92-jedicode <pcn92@mail.umkc.edu>
2014-04-06 21:51:30 -0500

    Hungry

--- wmw.js
+++ wmw.js
@@ -1,15 +1,27 @@
 //track keeptracking
 
-  var username = prompt('Say you Name');
+ // var username = prompt('Say you Name');
+  var trackid;
 
 
-var track = function(){
-  navigator.geolocation.watchPosition(function(position) {
-  var myDataRef = new Firebase('https://shining-fire-3947.firebaseio.com/'+ username +'/location');  
+
+var trackbegin = function(){
+  //if(track) console.log('True');
+  var myDataRef = new Firebase('https://shining-fire-3947.firebaseio.com/'+ username +'location');
+  
+  myDataRef.push($.now() + " "+ "Start");
+  trackid = navigator.geolocation.watchPosition(function(position) {
+    
   myDataRef.push($.now() + " " + position.coords.latitude + " "+ position.coords.longitude);
+  console.log("Tracking begin and Reporting to DB");
 });
-}
+};
 
+var trackstop = function(){
+  navigator.geolocation.clearWatch(trackid);
+  var myDataRef = new Firebase('https://shining-fire-3947.firebaseio.com/'+ username +'location');  
+  myDataRef.push($.now() + " " + "Stopped");
+}
 
 // geolocation
 var getlocation = function() {
